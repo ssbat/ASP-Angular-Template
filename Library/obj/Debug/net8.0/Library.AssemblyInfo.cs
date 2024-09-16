dd7f4d095c8d226c252688274c8699dd01d49fb3
unknown <saad_sbat@hotmail.com>
2024-09-16 23:52:18 +0200

    Second commit

--- Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
+++ Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38bd146e300810922b136df073e340a1df3a082c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ASP-Angular-Template
+
+## To Create a Migration
+### run this in the root folder
+dotnet ef migrations add Initial --startup-project LaptopShop --project Library --context AppDbContext
+## To Update the database
+dotnet ef database update --startup-project LaptopShop --project Library --context AppDbContext
